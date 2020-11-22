b86de0c404fede750497086dc632cc77c7c18aa7
Mateusz Tadla <mtadla@sii.pl>
2020-11-22 13:59:39 +0100

    cod efix

--- src/test/java/RegistrationTest.java
+++ src/test/java/RegistrationTest.java
@@ -1,10 +1,10 @@
 import org.testng.annotations.Test;
 import pages.HomePage;
 
-public class RegistrationPage extends TestBase {
+public class RegistrationTest extends TestBase {
 
     @Test
-    public void test() {
+    public void shouldCreateNewUser() {
         getDriver().get("http://5.196.7.235/");
         HomePage homePage = new HomePage(getDriver());
         homePage.getMenu().openLoginPage();
